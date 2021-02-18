7becb289469ee2d7d9d500b2391bfa93ed405ab7
JLeo <67970516+Hadron-JLeo@users.noreply.github.com>
2021-02-18 20:51:06 +0100

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,27 +1,22 @@
 
-
+/*
 window.addEventListener("load", addListeners);
+
+
+function addListeners() {
+    document.getElementById("testButton").addEventListener("click", addConsole);
+
+}
+
+*/
 
 var console_content = "";
 
-function addListeners() {
-    document.getElementById("testButton").addEventListener("click", do_fnct);
-
-}
-
-
-function do_fnct() {
-    alert("test");
-    print("hi")
-    //document.getElementById("Console").innerHTML = 5 + 6;
-}
-
 
 function addConsole() {
-    alert("fuk yes");
-    //console_content = document.getElementById("Console").innerHTML;
-    //console_content = console_content + " Hello";
-    //document.getElementById("Console").innerHTML = console_content;
+    console_content = document.getElementById("Console").innerHTML;
+    console_content = console_content + " Hello";
+    document.getElementById("Console").innerHTML = console_content;
 }
 
 function myFunction() {

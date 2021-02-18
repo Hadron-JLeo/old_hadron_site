f94dd1d6b65ffe1c354134c3f4d057d710fa8f6f
JLeo <67970516+Hadron-JLeo@users.noreply.github.com>
2021-02-18 20:56:27 +0100

    Update main.js

--- js/main.js
+++ js/main.js
@@ -10,16 +10,26 @@ function addListeners() {
 
 */
 
-var console_content = "";
-
-
 function addConsole() {
     console_content = document.getElementById("Console").innerHTML;
     console_content = console_content + " Hello";
     document.getElementById("Console").innerHTML = console_content;
 }
 
-function myFunction() {
-    alert("poop");
-
+function write_in_console(input) {
+    console_content = document.getElementById("Console").innerHTML;
+    console_content = console_content + "input";
+    
+    return console_content;
 }
+
+function start_bot() {
+    my_text = "Starting bot";
+    write_in_console(my_text);
+}
+
+function calculate_gas() {
+    var gas = 4;
+    write_in_console(gas + 5);
+}
+
